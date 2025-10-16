9ea356ae712d8eead92cee9d4ca3d7a5c0026007
konatenhamedm <konatenhamed@gmail.com>
2025-10-16 11:27:36 +0000

    save

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends AbstractController
         $professionnel = $professionnelRepository->findAll();
 
         foreach ($professionnel as $key => $value) {
-            $value->setProfession($professionneRepository->findOneBy(['code' => $value->getProfession()])->getId());
+            $value->setProfession($professionneRepository->findOneBy(['code' => $value->getProfession()]) ? $professionneRepository->findOneBy(['code' => $value->getProfession()])->getId() : null);
             $professionnelRepository->add($value,true);
         }
 
