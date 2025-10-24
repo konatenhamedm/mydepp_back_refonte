84049cc837fb54deae9c6153fdb5c55b42194a86
konatenhamedm <konatenhamed@gmail.com>
2025-10-24 16:14:38 +0000

    sve

--- src/Controller/Apis/ApiStatistiqueController.php
+++ src/Controller/Apis/ApiStatistiqueController.php
@@ -213,12 +213,12 @@ class ApiStatistiqueController extends ApiInterface
             // Calcul de la plage de dates
             [$startDate, $endDate] = $this->getDateRangeFromPeriode((int)$annee, $periode, (int)$mois, (int)$tranche);
 
+            dd($startDate,$endDate);
            // dd($startDate,$endDate,$annee,$mois,$tranche);
 
             // Requête optimisée sans filtres supplémentaires
             $stats2 = $professionnelRepository->findDiplomeStats($startDate, $endDate);
 
-            dd($stats2);
             //dd($startDate,$endDate,$annee);
 
 
