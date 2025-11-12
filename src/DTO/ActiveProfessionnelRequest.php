91ca67871ac0e00f770d0d5e3c343a03d3959e6a
konatenhamedm <konatenhamed@gmail.com>
2025-11-12 09:20:45 +0000

    save

--- src/DTO/ActiveProfessionnelRequest.php
+++ src/DTO/ActiveProfessionnelRequest.php
@@ -9,8 +9,8 @@ class ActiveProfessionnelRequest
 {
     #[Assert\NotBlank(message: "Le champ status est requis.")]
     #[Assert\Choice(
-        choices: ["acceptation", "rejet", "validation", "renouvellement", "mis_a_jour"],
-        message: "Le statut doit être l'une des valeurs suivantes : acceptation, rejet, validation, renouvellement, mis_a_jour."
+        choices: ["acceptation", "rejet","refuse", "validation", "renouvellement", "mis_a_jour"],
+        message: "Le statut doit être l'une des valeurs suivantes : acceptation, rejet,refuse, validation, renouvellement, mis_a_jour."
     )]
     public ?string $status = null;
 
